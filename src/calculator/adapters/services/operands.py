8f98a052b241f29c5f916d03ec27d22795132686
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-03-05 21:02:09 +0100

    feat: add context manager for factory call

--- src/calculator/adapters/services/operands.py
+++ src/calculator/adapters/services/operands.py
@@ -1,31 +1,31 @@
+from contextlib import contextmanager
 from decimal import Decimal
 
-from calculator.domain.model.model import operands_factory
+from calculator.domain.model.model import Operands, operands_factory
 from calculator.domain.model.schemas import OperandsCreateDTO
 from calculator.domain.ports.services.operands import OperandsServiceInterface
 
 
+@contextmanager
+def operands_factory_validation(left: Decimal, right: Decimal) -> Operands:
+    data_ = {"left": left, "right": right}
+    result = OperandsCreateDTO().load(data_)
+    yield operands_factory(**result)
+
+
 class OperandsService(OperandsServiceInterface):
     def _add(self, left: Decimal, right: Decimal) -> Decimal:
-        data_ = {"left": left, "right": right}
-        result = OperandsCreateDTO().load(data_)
-        operands = operands_factory(**result)
-        return operands.left + operands.right
+        with operands_factory_validation(left, right) as operands:
+            return operands.left + operands.right
 
     def _subtract(self, left: Decimal, right: Decimal) -> Decimal:
-        data_ = {"left": left, "right": right}
-        result = OperandsCreateDTO().load(data_)
-        operands = operands_factory(**result)
-        return operands.left - operands.right
+        with operands_factory_validation(left, right) as operands:
+            return operands.left - operands.right
 
     def _multiply(self, left: Decimal, right: Decimal) -> Decimal:
-        data_ = {"left": left, "right": right}
-        result = OperandsCreateDTO().load(data_)
-        operands = operands_factory(**result)
-        return operands.left * operands.right
+        with operands_factory_validation(left, right) as operands:
+            return operands.left * operands.right
 
     def _divide(self, left: Decimal, right: Decimal) -> Decimal:
-        data_ = {"left": left, "right": right}
-        result = OperandsCreateDTO().load(data_)
-        operands = operands_factory(**result)
-        return operands.left / operands.right
+        with operands_factory_validation(left, right) as operands:
+            return operands.left / operands.right
