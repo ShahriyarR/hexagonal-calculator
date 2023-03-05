1acfb0a0812e1e72eaa0ae0c31d53473230c95af
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-03-05 20:36:41 +0100

    test: add pytest fixture for operands service

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+from calculator.adapters.services.operands import OperandsService
+
+
+@pytest.fixture(scope="module")
+def get_operands_service():
+    return OperandsService()
--- tests/test_services.py
+++ tests/test_services.py
@@ -6,12 +6,10 @@ import pytest
 from calculator.adapters.services.operands import OperandsService
 
 
-def test_operands_service_add_result():
-    operand = OperandsService()
-    assert operand.add(Decimal("4.5"), Decimal("5")) == 9.5
+def test_operands_service_add_result(get_operands_service):
+    assert get_operands_service.add(Decimal("4.5"), Decimal("5")) == 9.5
 
 
-def test_operands_service_add_with_wrong_data_types():
-    operand = OperandsService()
+def test_operands_service_add_with_wrong_data_types(get_operands_service):
     with pytest.raises(marshmallow.exceptions.ValidationError):
-        operand.add([], Decimal("5")) == 9.5
+        get_operands_service.add([], Decimal("5")) == 9.5
