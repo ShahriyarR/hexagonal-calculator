d41fce69a0fc881d378ec5fd8452faa4c33fd1a7
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-03-30 19:25:22 +0200

    feat: add fake container and make migrations

--- src/calculator/configurator/settings.py
+++ src/calculator/configurator/settings.py
@@ -1,6 +1,13 @@
+import os
+
+
 class Settings:
     # Imagine this is a true production database connection
-    db_uri = "postgresql://postgres:postgres@localhost:5432/postgres"
+    db_uri = (
+        "sqlite:///tutorial.db"
+        if os.getenv("TEST_RUN")
+        else "postgresql://postgres:postgres@localhost:5432/postgres"
+    )
 
 
 def get_database_uri() -> str:
