c76b4cd4528040609e55a08f9a419852c15c6626
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-04-02 11:56:49 +0200

    feat: add subtract use case

--- tests/integrations/test_calculation_use_case.py
+++ tests/integrations/test_calculation_use_case.py
@@ -11,3 +11,15 @@ def test_calculate_use_case_add(get_fake_container, get_calculate_use_case):
                 assert len(result) == 1
                 assert result[0].action == "add"
                 assert result[0].result == 11
+
+
+def test_calculate_use_case_subtract(get_fake_container, get_calculate_use_case):
+    with Container.calculation_uow.override(get_fake_container.calculation_uow):
+        with Container.operands_service.override(get_fake_container.operands_service):
+            get_calculate_use_case.subtract(5, 6)
+            uow_ = get_fake_container.calculation_uow()
+            with uow_:
+                result = uow_.calculation.get_all()
+                assert len(result) == 2
+                assert result[1].action == "subtract"
+                assert result[1].result == -1
